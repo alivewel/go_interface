412a95296f9d8f3e94312b28979d197b04a36e09
vasilev.a178 <vasilev.a178@mail.ru>
2024-11-03 16:16:58 +0300

    add nil int

--- 11_nil_interface/main.go
+++ 11_nil_interface/main.go
@@ -15,12 +15,16 @@ func main() {
 	// type == nil, value == nil
 	// поэтому i == nil
 	fmt.Println(i == nil) // true
+	fmt.Printf("Value %v, type %T\n\n", i, i)
 
 	var d *Dog
+	// type == *Dog, value == nil
 	fmt.Println(d == nil) // true
+	fmt.Printf("Value %v, type %T\n\n", d, d)
 
 	i = d
 	// type == *Dog, value == nil
 	// поскольку type != nil, то i != nil
 	fmt.Println(i == nil) // false
+	fmt.Printf("Value %v, type %T\n", i, i)
 }
